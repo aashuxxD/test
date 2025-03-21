927dc6bc29db627646f1e2f2a0670bb3ec5c2207
Saykkuno (Ashutosh) <130449575+aashuxxD@users.noreply.github.com>
2025-03-21 15:08:07 -0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -55,7 +55,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/aashuxxD/test",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "uni")
 
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", None)
